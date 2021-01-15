bb629d09c535ebe40b8297ab4901328829823a92
EMRAgit <49976179+EMRAgit@users.noreply.github.com>
2021-01-15 20:47:44 +0100

    Update fDoyCrit.R

--- function/fDoyCrit.R
+++ function/fDoyCrit.R
@@ -7,8 +7,9 @@ fDoyCrit<-function(TEMP.PHENO,
                    Q1,
                    Q2){
   
+  
+  ### Import result from function "fTeffSum"
   temps_int_pheno <- TEMP.PHENO
-  head(TEMP.PHENO)
   
   ### Cumulative sums from start DOY to maximum DOY
   cumsums <- temps_int_pheno
@@ -27,6 +28,8 @@ fDoyCrit<-function(TEMP.PHENO,
     quantiles <- data.frame(Q=seq(from=Q1, to=Q2, by=0.05),R=0,RMSE=0,ME=0)
     
     ## Quantile determination
+    
+    print("Calculating quantile variants")
     total <- nrow(quantiles)
     pq <- txtProgressBar(min=0, max=total, style=3)
     for(q in seq(1,nrow(quantiles))){
@@ -48,10 +51,12 @@ fDoyCrit<-function(TEMP.PHENO,
       
       ### Appending to temporal temps_int_pheno
       temps_int_pheno_opt <- temps_int_pheno
+      temps_int_pheno_opt$DOY_PHASE
       
       ### Error filtering (Inf and outliers)
       temps_int_pheno_opt <- temps_int_pheno_opt[which(abs(temps_int_pheno_opt$DOY_PHASE-temps_int_pheno_opt$DOY) <= F.STD*sd(temps_int_pheno_opt$DOY)),]
 
+      
       ### Calculation of statistics
       quantiles$R[q] <- cor(temps_int_pheno_opt@data["DOY"],temps_int_pheno_opt@data["DOY_PHASE"],method="pearson")
       quantiles$RMSE[q] <- sqrt(mean((temps_int_pheno_opt$DOY-temps_int_pheno_opt$DOY_PHASE)^2))
@@ -59,7 +64,7 @@ fDoyCrit<-function(TEMP.PHENO,
       
       
       ### Comparison of statistics
-      if(quantiles$ME[q] < ME_MIN) {
+      if(quantiles$ME[q] < ME_MIN){
         temps_int_pheno_final <- temps_int_pheno_opt
         ME_MIN <- quantiles$ME[q]
         Q_MIN <- quantiles$Q[q]
@@ -68,11 +73,12 @@ fDoyCrit<-function(TEMP.PHENO,
       setTxtProgressBar(pq, q)
     }
 
-    write.csv2(quantiles,
-               row.names = FALSE,
-               paste(OUT.DIR,'QOPT',PLANT,"-",PHASE,"_",YEAR,"_F",F.STD*10,".csv",sep=""))
-    close(pq)
+    ### Export quantile-specific correlations results and filtered observation shapefile
+    #write.csv2(quantiles,
+    #           row.names = FALSE,
+    #           paste(OUT.DIR,"OPT_",PLANT,"-",PHASE,"_",YEAR,"_FSTD",F.STD,".csv",sep=""))
+    
     temps_int_pheno <- temps_int_pheno_final
-    head(temps_int_pheno)
-  return(temps_int_pheno)
+    #shapefile(temps_int_pheno,paste(OUT.DIR,"DOY_",PLANT,"-",PHASE,"_",YEAR,"_FSTD",F.STD,".shp",sep=""),overwrite=TRUE)
+    return(list(QuantileAccuracy = quantiles, FilterShape = temps_int_pheno))
 }
